e778b7863bc9bfb4aa641b4f1c1a39269145ea43
pmartelletti@gmail.com <pmartelletti@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-04-21 16:33:12 +0000

    Agrego la clase Grafo. Faltan algunos metodos del TAD que no utilizariamos para el TP, y las pruebas del grafo.

--- trunk/tp2/tp2/src/Ejercicios/Grafo.java
+++ trunk/tp2/tp2/src/Ejercicios/Grafo.java
@@ -1,35 +1,69 @@
 package Ejercicios;
 
+import java.util.Hashtable;
+
 public class Grafo {
 
-	public Hastable<String,Vertice> Vertices;
+	private Hashtable<String,Vertice> Vertices;
 
-	public function Grafo() {
-		this.Vertices = new Hastable();
+	public Grafo() {
+		this.Vertices = new Hashtable<String, Grafo.Vertice>();
+	}
+	
+	public Integer getPesoArista(Vertice v1, Vertice v2) {
+		return v1.getPesoArista(v2.getDato());
+	}
+	
+	public void insertarVertice(String clave, String dato){
+		Vertice v = new Vertice(dato);
+		this.Vertices.put(clave, v);
+	}
+	
+	public Vertice obtenerVertice(String clave) {
+		return this.Vertices.get(clave);
+	}
+	
+	public boolean agregarArista(String v1, String v2, Integer peso) {
+		if (this.obtenerVertice(v1) != null && this.obtenerVertice(v2) != null) {
+			this.Vertices.get(v1).agregarArista(v2, peso);
+			this.Vertices.get(v2).agregarArista(v1, peso);
+			return true;
+		}
+		return false;
+	}
+	
+	public Hashtable<String,Vertice> obtenerListaVertices() {
+		return this.Vertices;
+	}
+	
+	public Hashtable<String,Integer> obtenerVerticesAdyacentes(String vertice){
+		return this.Vertices.get(vertice).getVerticesAdyacentes();
 	}
 
 	private class Vertice {
-		public Object Dato; // que tipo de dato deberia ser?
-		public Hashtable adyacentes;
+		public String Dato; // que tipo de dato deberia ser?
+		public Hashtable<String, Integer> adyacentes;
 		
-		public function Vertice() {
-			this.Dato = new Object();
-			this.adyacentes = new Hashtable();
+		public Vertice(String dato) {
+			this.Dato = new String(dato);
+			this.adyacentes = new Hashtable<String, Integer>();
 		}
 		
-		public function getVerticesAdyacentes() {
+		public Hashtable<String, Integer> getVerticesAdyacentes() {
 			return this.adyacentes;
 		}
 		
-		public function getPesoArista(Vertice v) {
-			return this.adyacentes.getKey(v.Dato)
+		public void agregarArista(String clave, Integer peso) {
+			this.adyacentes.put(clave, peso);
+		}
+		
+		public Integer getPesoArista(String vecino) {
+			return this.adyacentes.get(vecino);
+		}
+		
+		public String getDato() {
+			return this.Dato;
 		}
 	}
-	
-	public function getPesoArista(Vertice v1, Vertice v2) {
-		return v1.getPesoArista(v2);
-	}
-
-	
 
 }
