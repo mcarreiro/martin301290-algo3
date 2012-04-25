43f4becb98440d1679670d3f9f096014eeb70681
pmartelletti@gmail.com <pmartelletti@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-04-25 00:37:43 +0000

    TAD Grafo con pruebas, y metodo con algoritmo de Prim para generar un AGM

--- tp2/tp2/src/Tests/TestGrafo.java
+++ tp2/tp2/src/Tests/TestGrafo.java
@@ -1,13 +1,21 @@
 package Tests;
 
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 
 import junit.framework.TestCase;
 
 import Ejercicios.Grafo;
+import Ejercicios.Grafo.Arista;
 import Ejercicios.Grafo.Vertice;
 
 public class TestGrafo extends TestCase {
+	
+	public void testGetVertices() {
+		//Grafo g = new Grafo();
+		
+	}
 
 	public void testInsertarVertice() {
 		Grafo g = new Grafo();
@@ -53,6 +61,54 @@ public class TestGrafo extends TestCase {
 		assertEquals(g.getPesoArista("C1", "C2"), new Integer(20));
 		assertEquals(g.getPesoArista("C1", "C3"), new Integer(10));
 		assertEquals(g.getPesoArista("C3", "C2"), new Integer(15));		
+	}
+	
+	public void testGetAristas() {
+		Grafo g = new Grafo();
+		g.insertarVertice("C1", "1");
+		g.insertarVertice("C2", "1");
+		g.insertarVertice("C3", "3");
+		g.agregarArista("C1", "C2", new Integer(20));
+		g.agregarArista("C1", "C3", new Integer(30));
+		g.agregarArista("C2", "C3", new Integer(15));
+		PriorityQueue<Arista> cola = g.getAristas();
+		assertEquals(cola.size(), 3);
+	}
+	
+	public void testCrearAgm() {
+		Grafo g = new Grafo();
+		g.insertarVertice("C1", "1");
+		g.insertarVertice("C2", "1");
+		g.insertarVertice("C3", "3");
+		g.agregarArista("C1", "C2", new Integer(20));
+		g.agregarArista("C1", "C3", new Integer(30));
+		g.agregarArista("C2", "C3", new Integer(15));
+		//@SuppressWarnings("unused")
+		//Grafo agm = g.getArbolRecubridoMinimo();
+	}
+	
+	public void testCrearAgm2() {
+		Grafo g = new Grafo();
+		g.insertarVertice("A", "A");
+		g.insertarVertice("B", "B");
+		g.insertarVertice("C", "C");
+		g.insertarVertice("D", "D");
+		g.insertarVertice("E", "E");
+		g.insertarVertice("F", "F");
+		g.insertarVertice("G", "G");
+		g.agregarArista("A", "B", 7);
+		g.agregarArista("A", "D", 5);
+		g.agregarArista("D", "F", 6);
+		g.agregarArista("D", "E", 15);
+		g.agregarArista("F", "E", 8);
+		g.agregarArista("F", "G", 11);
+		g.agregarArista("E", "G", 9);
+		g.agregarArista("C", "E", 5);
+		g.agregarArista("B", "E", 7);
+		g.agregarArista("B", "C", 8);
+		@SuppressWarnings("unused")
+		Grafo agm = g.getArbolRecubridoMinimo();
+		
 	}
 	
 }
