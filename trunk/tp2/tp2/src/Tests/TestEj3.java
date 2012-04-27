20ad2f3943ef60b31e0a85872cbb8cd648e0e8d6
kevinkuja@gmail.com <kevinkuja@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-04-27 15:15:13 +0000

    Ej3, hay un errorcito que no descubro todavia

---
+++ trunk/tp2/tp2/src/Tests/TestEj3.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import java.io.Console;
+
+import junit.framework.TestCase;
+
+import Ejercicios.Ej3;
+
+public class TestEj3  extends TestCase {
+	public void test(){
+		Ej3 ej3 = new Ej3();
+		String nodos[]= {"1","3","4","5","7","10"};
+		Boolean res = ej3.Resolver(nodos, 4, 7, 3, 5);
+		System.out.println(res);
+	}
+	public void test1(){
+		Ej3 ej3 = new Ej3();
+		String nodos[]= {"1","2","4","8","10"};
+		Boolean res = ej3.Resolver(nodos, 4, 8, 3, 5);
+		Boolean res1 = ej3.Resolver(nodos, 4, 2, 3, 5);
+		Boolean res2 = ej3.Resolver(nodos, 4, 1, 3, 5);
+		System.out.println(res);
+		System.out.println(res1);
+		System.out.println(res2);
+	}
+}
