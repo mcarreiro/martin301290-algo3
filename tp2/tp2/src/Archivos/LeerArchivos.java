7d30e10401183da0647c672c05fe6f90893c9f00
kevinkuja@gmail.com <kevinkuja@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-04-27 20:42:14 +0000

    Clases para leer y escribir en archivos

---
+++ tp2/tp2/src/Archivos/LeerArchivos.java
@@ -0,0 +1,42 @@
+package Archivos;
+import java.io.*;
+import java.util.*;
+public class LeerArchivos {
+	public static ArrayList<String> leer (String dir) {
+	      File archivo = null;
+	      FileReader fr = null;
+	      BufferedReader br = null;
+	      ArrayList<String>  result = new ArrayList<String>();
+	      try {
+	         // Apertura del fichero y creacion de BufferedReader para poder
+	         // hacer una lectura comoda (disponer del metodo readLine()).
+	         archivo = new File (dir);
+	         fr = new FileReader (archivo);
+	         br = new BufferedReader(fr);
+
+	         // Lectura del fichero
+	         String linea;	        
+	         while((linea=br.readLine())!=null){
+	        	result.add(linea);	        		        	
+	         }
+	      }
+	      catch(Exception e){
+	         e.printStackTrace();
+	      }
+	      // En el finally cerramos el fichero, para asegurarnos
+	      // que se cierra tanto si todo va bien como si salta 
+	      // una excepcion.
+	      try{                    
+	         if( null != fr ){   
+	            fr.close();     
+	         }                  
+	      }catch (Exception e2){ 
+	         e2.printStackTrace();
+	      }
+	      return result;
+	      
+	      
+	      
+	   }
+
+}
