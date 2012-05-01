90516d4c8c743bfeafcee2a505a13392c2aaad5e
pmartelletti@gmail.com <pmartelletti@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-05-01 19:54:11 +0000

    Ejercicio 2 con AGM (algoritmo de Prim) optimizado.

--- tp2/tp2/src/Ejercicios/Grafo.java
+++ tp2/tp2/src/Ejercicios/Grafo.java
@@ -2,8 +2,6 @@ package Ejercicios;
 
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -13,140 +11,150 @@ import java.util.PriorityQueue;
 public class Grafo {
 
 	private Hashtable<String,Vertice> Vertices;
-	private PriorityQueue<Arista> Aristas;
+	private ArrayList<Arista> aristas;
 
 	public Grafo() {
 		this.Vertices = new Hashtable<String, Grafo.Vertice>();
-		this.Aristas = new PriorityQueue<Grafo.Arista>();
+		this.aristas = new ArrayList<Grafo.Arista>();
+	}
+	
+	public String toString() {
+		String res = "";
+		Iterator<Arista> i = this.aristas.iterator();
+		while(i.hasNext()) res += i.next() + ";";
+		return res;
 	}
 	
 	public Hashtable<String,Vertice> getVertices() {
 		return this.Vertices;
 	}
 	
-	public void insertarVertice(String clave, String dato){
+	public Vertice insertarVertice(String clave, String dato){
 		Vertice v = new Vertice(dato);
 		this.Vertices.put(clave, v);
+		return v;
+	}
+	
+	/**
+	 * Método para insertar vértice a partir de un vértice ya creado
+	 * @param v
+	 */
+	public Vertice insertaVertice(Vertice v) {
+		Vertice aux = new Vertice(v.Dato);
+		this.Vertices.put(v.Dato, aux );
+		return aux;
 	}
 	
 	public Vertice obtenerVertice(String clave) {
 		return this.Vertices.get(clave);
 	}
 	
-	public boolean agregarArista(String v1, String v2, Integer peso) {
-		if (this.obtenerVertice(v1) != null && this.obtenerVertice(v2) != null) {
-			this.Vertices.get(v1).agregarArista(v2, peso);
-			this.Vertices.get(v2).agregarArista(v1, peso);
-			this.Aristas.add(new Arista(v1, v2, peso)); 
-			return true;
-		}
-		return false;
-	}
-	
-	public Hashtable<String,Vertice> obtenerListaVertices() {
-		return this.Vertices;
-	}
-	
-	public HashSet<Arista> obtenerVerticesAdyacentes(String vertice){
-		return this.obtenerVertice(vertice).getVerticesAdyacentes();
-	}
-	
-	public PriorityQueue<Arista> getAristas() {
-		return this.Aristas;
-	}
+	public void agregarArista(Vertice v1, Vertice v2, Integer peso) {
+		v1.agregarArista(v2, peso);
+		v2.agregarArista(v1, peso);
+		this.aristas.add(new Arista(v1, v2, peso));
+	}	
 	
 	public boolean hasVertex(String v){
 		return this.Vertices.containsKey(v);
 	}
-	
-	/**
-	 * Dada una instancia de un grafo, devuelve el arbol recubridor minimo (que es un grafo)
-	 * utilizando el algoritmo de o prim (decidir)
-	 * @return Grafo arbol recubridor minimo
-	 */
-	public Grafo getArbolRecubridoMinimo(){
-		Hashtable<String, Vertice> v = ((Hashtable<String,Vertice>)this.getVertices().clone());
-		int initialSize = v.size();
-		PriorityQueue<Arista> aristasGrafo = this.getAristas();
-		Grafo agm = new Grafo();
-		// agrego la arista de menor peso
-		Arista aristaMin = aristasGrafo.poll();
-		// agrego los vertices de la arista a la lista de vertices
-		agm.insertarVertice(aristaMin.v1, aristaMin.v1);
-		agm.insertarVertice(aristaMin.v2, aristaMin.v2);
-		agm.agregarArista(aristaMin.v1, aristaMin.v2, aristaMin.peso);
-		// elimino los vertices de la lista de vertices
-		v.remove(aristaMin.v1);
-		v.remove(aristaMin.v2);
-		while(agm.getVertices().size() < initialSize ){
-			ArrayList<Arista> aristaSinUsar = new ArrayList<Grafo.Arista>();
-			// desencolo una arista de peso minimo
-			Hashtable<String, Vertice> verticesAgm = agm.getVertices();
-			Arista a = aristasGrafo.poll();
-			// me fijo si a tiene un extremo en this y otro en agm 
-			String v1 = a.v1;
-			String v2 = a.v2;
-			while( !(this.hasVertex(v1) && agm.hasVertex(v2)) && 
-					!(this.hasVertex(v2) && agm.hasVertex(v1))  ){
-				aristaSinUsar.add(a);
-				if(aristasGrafo.isEmpty()) break;
-				a = aristasGrafo.poll();
-				v1 = a.v1;
-				v2 = a.v2;
-			}
-			// de ser asi, agrego el vertice y la arista a agm
-			if ( (v.containsKey(v1) && !v.containsKey(v2)) && 
-					(verticesAgm.containsKey(v2) && !verticesAgm.containsKey(v1) )) {
-				v.remove(v1);
-				agm.insertarVertice(v1, v1);
-				agm.agregarArista(v1, v2, a.peso);
-			}
-			if ( (v.containsKey(v2) && !v.containsKey(v1)) && 
-					(verticesAgm.containsKey(v1) && !verticesAgm.containsValue(v2)) ) {
-				v.remove(v2);
-				agm.insertarVertice(v2, v1);
-				agm.agregarArista(v1, v2, a.peso);
-			}
-			Iterator<Arista> i = aristaSinUsar.iterator();
-			while(i.hasNext()) aristasGrafo.add(i.next());
-		}
-		return agm;
-	}
 
+	public Grafo getAgm() {
+		Grafo g = new Grafo();
+		// saco un vertice al azar y lo agrego al árbol
+		Iterator<Vertice> j = this.getVertices().values().iterator();
+		Vertice v1 = j.next();
+		Vertice v2;
+		g.insertaVertice(v1);
+		PriorityQueue<Arista> aristas = new PriorityQueue<Grafo.Arista>();
+		aristas.addAll(v1.getAristas());
+		v1.marcarVisitado();
+		Arista min;
+		// mientras que el agm no tenga todos los nodos, sigo agregando
+		while(g.getVertices().size() < this.getVertices().size() ){
+			// agrego las aristas de los nuevos vertices
+			min = aristas.poll();
+			v1 = min.v1;
+			v2 = min.v2;
+			if ( !( v1.fueVisitado() ) ){
+				g.agregarArista(g.insertaVertice(v1), v2, min.getPeso());
+				v1.marcarVisitado();
+				aristas.addAll(v1.getAristas());
+			}
+			if(!( v2.fueVisitado() )){
+				g.agregarArista(g.insertaVertice(v2), v1, min.getPeso());
+				v2.marcarVisitado();
+				aristas.addAll(v2.getAristas());
+			}
+		}
+		return g;
+	}
+	
+	public Vertice insertauObtenerVertice(String clave, String dato) {
+		Vertice v = this.obtenerVertice(clave);
+		if ( v == null ) {
+			v = this.insertarVertice(clave, dato);
+		}
+		return v;
+	}
 	
 	
 	public class Vertice {
 		public String Dato; // que tipo de dato deberia ser?
-		public HashSet<Arista> adyacentes;
+		public ArrayList<Arista> aristas;
+		public boolean visitado;
 		
 		public Vertice(String dato) {
 			this.Dato = new String(dato);
-			this.adyacentes = new HashSet<Grafo.Arista>(); // modificar esto
+			this.aristas = new ArrayList<Grafo.Arista>(); // modificar esto
+			this.visitado = false;
 		}
 		
-		public HashSet<Arista> getVerticesAdyacentes() {
-			return this.adyacentes;
+		public String toString() {
+			return this.Dato;
+		}
+		
+		public ArrayList<Arista> getAristas() {
+			return this.aristas;
 		}
 		
 		
-		public void agregarArista(String v, Integer peso) {
-			this.adyacentes.add(new Arista(this.Dato, v, peso));
+		public void agregarArista(Vertice v, Integer peso) {
+			this.aristas.add(new Arista(this, v, peso));
 		}
 		
 		public String getDato() {
 			return this.Dato;
 		}
+		
+		public boolean fueVisitado() {
+			return this.visitado;
+		}
+		
+		public void marcarVisitado() {
+			this.visitado = true;
+		}
+		
+		public void desmarcarVisitado() {
+			this.visitado = false;
+		}
 	}
 	
 	public class Arista implements Comparable<Arista>{
-		public String v1;
-		public String v2;
+		public Vertice v1;
+		public Vertice v2;
 		public Integer peso;
+		public boolean agregada;
 		
-		public Arista(String v1, String v2, Integer p) {
+		public Arista(Vertice v1, Vertice v2, Integer p) {
 			this.v1 = v1;
 			this.v2 = v2;
 			this.peso = p;
+			this.agregada = false;
+		}
+		
+		public Integer getPeso() {
+			return this.peso;
 		}
 
 		public int compareTo(Arista that) {
@@ -154,7 +162,19 @@ public class Grafo {
 		}
 		
 		public String toString() {
-			return new String("<" + this.v1 + "," + this.v2 + "> | Peso: " + this.peso);
+			return new String("" + this.v1 + " " + this.v2 + " " + this.peso);
+		}
+		
+		public boolean fueAgregada() {
+			return this.agregada;
+		}
+		
+		public void marcarAgregada() {
+			this.agregada = true;
+		}
+		
+		public void desmarcarAgregada() {
+			this.agregada = false;
 		}
 		
 		
