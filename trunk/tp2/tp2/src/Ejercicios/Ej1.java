5b1317a556498eb2d194a3a95dda5a38b5ea94f4
jmanuoz@gmail.com <jmanuoz@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-05-01 20:01:49 +0000

    un poco mas del 1

--- trunk/tp2/tp2/src/Ejercicios/Ej1.java
+++ trunk/tp2/tp2/src/Ejercicios/Ej1.java
@@ -11,19 +11,32 @@ public class Ej1 {
 		for(int i = (pisos.size() - 1); i >= 0 ;i--){
 			energiaAux = energia;
 			cantPersonasAux = 0;
-			pisosAux = pisos;
+			pisosAux = (ArrayList< Integer >)pisos.clone();
+			//levanto de cada piso de forma tal q me llene la capacidad
 			for(int a=i; a>=0; a--){
 				if(energiaAux >= ((a+1)*2)){
-					energiaAux = energiaAux - ((a+1)*2);
-					if(pisosAux.get(a) > capacidad){
+					
+					if(pisosAux.get(a) >= capacidad){
+						energiaAux = energiaAux - ((a+1)*2);
 						cantPersonasAux += capacidad;						
 						pisosAux.set(a, pisosAux.get(a) - capacidad);
 						a++;
 					}else{
-						cantPersonasAux += pisosAux.get(a);
+						//cantPersonasAux += pisosAux.get(a);
 					}
 				}else{
 					break;
+				}
+			}
+			//levanto lo que queda (que ya se que no me llena la capacidad)
+			for(int a=i; a>=0; a--){
+				if(energiaAux >= ((a+1)*2)){					
+					if(pisosAux.get(a) > 0){
+						energiaAux = energiaAux - ((a+1)*2);
+						cantPersonasAux += pisosAux.get(a);						
+						pisosAux.set(a, pisosAux.get(a) - capacidad);
+						//aca deberia levantar de los pisos que hay abajo de forma optima
+					}
 				}
 			}
 			if(cantPersonasAux > cantPersonas){
