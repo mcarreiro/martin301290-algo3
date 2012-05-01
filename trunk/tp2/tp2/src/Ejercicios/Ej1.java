e277d961953f07b9589f8353a4e2d4476906246b
jmanuoz@gmail.com <jmanuoz@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-05-01 18:24:40 +0000

    sadsadsad

--- trunk/tp2/tp2/src/Ejercicios/Ej1.java
+++ trunk/tp2/tp2/src/Ejercicios/Ej1.java
@@ -1,5 +1,35 @@
 package Ejercicios;
 
-public class Ej1 {
+import java.util.*;
 
+public class Ej1 {
+	public int resolver(int energia, int capacidad, ArrayList< Integer > pisos){
+		int cantPersonas = 0;
+		int cantPersonasAux = 0;
+		int energiaAux;
+		ArrayList< Integer > pisosAux;
+		for(int i = (pisos.size() - 1); i >= 0 ;i--){
+			energiaAux = energia;
+			cantPersonasAux = 0;
+			pisosAux = pisos;
+			for(int a=i; a>=0; a--){
+				if(energiaAux >= ((a+1)*2)){
+					energiaAux = energiaAux - ((a+1)*2);
+					if(pisosAux.get(a) > capacidad){
+						cantPersonasAux += capacidad;						
+						pisosAux.set(a, pisosAux.get(a) - capacidad);
+						a++;
+					}else{
+						cantPersonasAux += pisosAux.get(a);
+					}
+				}else{
+					break;
+				}
+			}
+			if(cantPersonasAux > cantPersonas){
+				cantPersonas = cantPersonasAux;
+			}
+		}
+		return cantPersonas;
+	}
 }
