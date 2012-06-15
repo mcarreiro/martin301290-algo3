5db1fe9f59eae0e0349b8409669d6dce8b9f1517
kevinkuja@gmail.com <kevinkuja@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-06-15 23:27:27 +0000

    ej4 en pseudocodigo con consultas realizadas

---
+++ trunk/tp3/src/ejercicios/ej4.java
@@ -0,0 +1,31 @@
+package ejercicios;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import ejercicios.Grafo.Vertice;
+
+public class ej4 {
+
+	//static Collection<Grafo.Vertice> grafoOriginal;
+	
+	public static Grafo MCD_LocalSearch(Grafo g){
+		Ej3 ej3 = new Ej3();
+		ArrayList<Vertice> greedySolution = ej3.MCD_Greedy(g); //Nodos dominados
+		Hasta tuveQueAgregar > k
+			ListaTupladeVertices ObtenerParesDeNodosOrdenadosPorFuncionF;
+				si se peude hacer 2x1  
+					tuveQueAGregar = 0;
+					cambiar 2x1;//No hay que chequear que sea dominante porque el se puede ya te lo da, calcuar N(x,y) = N(z)
+				si no
+					i = random
+					ArregloFunciones funcion(i); //MEDIO TRUCHO
+						*agregar 1 nodo de los no dominantes
+						*quitar un nodod en caso de poder
+						*cambiar un nodo dominante por otro no dominante y que siga siendo dominante
+					tuveQueAGregar++;		
+	}
+	
+	
+	
+}
