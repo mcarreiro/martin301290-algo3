6537c2e475f06e57cdebbe22ca21dae56d8315f2
kevinkuja@gmail.com <kevinkuja@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-06-20 15:43:27 +0000

    ej3, no ordena, elige el mejor.

--- trunk/tp3/src/Tests/TestEj3.java
+++ trunk/tp3/src/Tests/TestEj3.java
@@ -23,4 +23,34 @@ public class TestEj3 extends TestCase{
 		g.agregarArista(b, f, 0);
 		List<Grafo.Vertice> conj = ej3.MCD_Greedy(g);
 	}
+	
+	public void test2() {
+		Ej3 ej3 = new Ej3();
+		Grafo grafo = new Grafo();
+		Grafo.Vertice a = grafo.insertarVertice("A", "A");
+		Grafo.Vertice b = grafo.insertarVertice("B", "B");
+		Grafo.Vertice c = grafo.insertarVertice("C", "C");
+		Grafo.Vertice d = grafo.insertarVertice("D", "D");
+		Grafo.Vertice e = grafo.insertarVertice("E", "E");
+		Grafo.Vertice f = grafo.insertarVertice("F", "F");
+		Grafo.Vertice g = grafo.insertarVertice("G", "G");
+		Grafo.Vertice h = grafo.insertarVertice("H", "H");
+		Grafo.Vertice i = grafo.insertarVertice("I", "I");
+		Grafo.Vertice j = grafo.insertarVertice("J", "J");
+		Grafo.Vertice k = grafo.insertarVertice("K", "K");
+		Grafo.Vertice l = grafo.insertarVertice("L", "L");
+		grafo.agregarArista(a, b, 0);
+		grafo.agregarArista(a, c, 0);
+		grafo.agregarArista(b, d, 0);
+		grafo.agregarArista(a, e, 0);
+		grafo.agregarArista(b, f, 0);
+		grafo.agregarArista(g, f, 0);
+		grafo.agregarArista(g, h, 0);
+		grafo.agregarArista(g, i, 0);
+		grafo.agregarArista(h, k, 0);
+		grafo.agregarArista(k, l, 0);
+		grafo.agregarArista(h, l, 0);
+
+		List<Grafo.Vertice> conj = ej3.MCD_Greedy(grafo);
+	}
 }
