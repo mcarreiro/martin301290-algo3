a2028e8ff243b5359ebfee89eaa1e6d9a777f551
kevinkuja@gmail.com <kevinkuja@gmail.com@72346fcb-e063-4215-32f1-643b8954bda2>
2012-06-27 06:16:48 +0000

    Instancias generadas por el script de tincheriolo

--- tp3/src/Tests/TestEj3.java
+++ tp3/src/Tests/TestEj3.java
@@ -168,4 +168,77 @@ public class TestEj3 extends TestCase{
         System.out.print(Ej3.ciclos);
         
 	}
+
+public void testInstancias(){
+
+		ArrayList<String>  instancias = new ArrayList<String>();
+		instancias.add("2 2 0;1 1 0");
+		instancias.add("3 3 0;1 3 0;3 1 0");
+		instancias.add("2 2 0;2 4 0;2 1 0;1 1 0");
+		instancias.add("1 2 0;2 5 0;4 4 0;5 5 0;2 2 0");
+		instancias.add("4 5 0;5 1 0;3 2 0;6 4 0;1 1 0;5 4 0");
+		instancias.add("1 5 0;7 3 0;5 3 0;4 6 0;3 1 0;4 4 0;3 2 0");
+		instancias.add("8 7 0;3 1 0;6 5 0;7 1 0;3 7 0;5 8 0;7 6 0;4 8 0");
+		instancias.add("2 9 0;4 7 0;7 6 0;8 8 0;2 8 0;3 7 0;8 9 0;6 4 0;8 1 0");
+		instancias.add("1 2 0;10 7 0;2 6 0;6 10 0;7 5 0;2 1 0;3 4 0;6 10 0;8 6 0;6 7 0");
+		instancias.add("2 3 0;10 8 0;4 3 0;4 10 0;9 9 0;6 9 0;10 11 0;3 11 0;10 8 0;4 7 0;4 6 0");
+		instancias.add("9 2 0;1 6 0;9 4 0;5 6 0;3 1 0;7 6 0;11 7 0;3 8 0;3 12 0;1 4 0;1 11 0;8 12 0");
+		instancias.add("9 11 0;10 6 0;11 3 0;5 11 0;1 4 0;7 10 0;6 5 0;13 7 0;9 3 0;10 5 0;12 9 0;5 4 0;10 3 0");
+		instancias.add("2 13 0;2 12 0;6 10 0;14 12 0;6 9 0;7 14 0;5 11 0;11 1 0;1 4 0;11 11 0;7 5 0;3 3 0;6 1 0;5 11 0");
+		instancias.add("12 3 0;2 13 0;13 10 0;12 6 0;12 14 0;3 8 0;9 8 0;2 15 0;10 5 0;8 7 0;13 9 0;10 13 0;6 2 0;15 13 0;3 15 0");
+		instancias.add("3 1 0;14 1 0;3 6 0;10 3 0;1 3 0;1 2 0;10 4 0;4 16 0;13 10 0;12 6 0;14 15 0;11 9 0;13 11 0;5 9 0;15 11 0;9 16 0");
+		instancias.add("14 2 0;16 10 0;13 4 0;9 5 0;4 11 0;4 4 0;12 16 0;12 9 0;2 10 0;16 7 0;13 9 0;5 17 0;12 6 0;4 6 0;9 14 0;15 10 0;6 7 0");
+		instancias.add("11 18 0;8 8 0;11 15 0;14 7 0;8 7 0;7 7 0;5 7 0;4 7 0;6 8 0;16 13 0;16 7 0;11 13 0;2 8 0;6 9 0;6 7 0;4 7 0;1 4 0;7 2 0");
+		instancias.add("16 19 0;2 13 0;10 17 0;16 2 0;3 2 0;9 5 0;1 12 0;10 15 0;15 13 0;17 5 0;13 7 0;14 16 0;11 18 0;10 7 0;19 6 0;1 19 0;5 13 0;11 5 0;10 11 0");
+		instancias.add("1 13 0;17 16 0;15 18 0;10 3 0;15 15 0;18 7 0;17 3 0;9 12 0;15 15 0;9 2 0;7 17 0;10 8 0;11 2 0;14 8 0;9 6 0;12 17 0;15 7 0;9 3 0;1 8 0;2 13 0");
+		instancias.add("4 18 0;1 13 0;15 12 0;20 10 0;21 11 0;5 3 0;4 19 0;20 4 0;3 6 0;4 13 0;20 9 0;7 9 0;5 19 0;16 16 0;17 20 0;17 4 0;11 1 0;3 4 0;19 20 0;9 12 0;16 12 0");
+		instancias.add("7 9 0;5 11 0;14 4 0;9 2 0;1 4 0;22 9 0;1 22 0;8 4 0;4 21 0;16 16 0;8 4 0;5 8 0;3 1 0;11 15 0;13 16 0;15 20 0;9 15 0;10 17 0;6 3 0;2 4 0;21 21 0;4 9 0");
+		instancias.add("9 1 0;15 2 0;22 11 0;20 23 0;5 14 0;11 17 0;4 21 0;19 19 0;21 12 0;4 20 0;13 15 0;9 8 0;2 11 0;4 7 0;7 2 0;6 12 0;3 22 0;7 17 0;23 9 0;17 3 0;20 1 0;7 21 0;14 8 0");
+		instancias.add("3 18 0;9 3 0;13 24 0;21 19 0;13 11 0;10 13 0;21 15 0;11 15 0;13 2 0;13 24 0;17 6 0;7 17 0;17 20 0;5 21 0;10 8 0;16 11 0;21 5 0;10 21 0;14 19 0;4 18 0;24 2 0;7 24 0;14 20 0;6 9 0");
+		instancias.add("6 13 0;13 16 0;18 1 0;6 22 0;2 2 0;9 24 0;18 16 0;16 22 0;21 13 0;15 21 0;24 6 0;25 8 0;17 11 0;10 10 0;9 21 0;19 16 0;10 7 0;3 21 0;4 11 0;14 9 0;17 3 0;7 25 0;10 13 0;25 25 0;15 2 0");
+		instancias.add("26 23 0;9 20 0;8 9 0;16 20 0;26 8 0;10 13 0;16 3 0;5 8 0;19 14 0;4 13 0;8 21 0;24 19 0;12 24 0;15 9 0;5 20 0;2 23 0;14 22 0;8 26 0;15 18 0;26 11 0;18 9 0;5 9 0;23 23 0;16 8 0;7 1 0;10 14 0");
+		instancias.add("22 8 0;12 5 0;27 12 0;21 13 0;14 23 0;26 14 0;21 24 0;20 18 0;20 20 0;26 11 0;15 16 0;24 27 0;20 2 0;2 11 0;6 22 0;24 10 0;27 8 0;13 16 0;1 12 0;12 3 0;24 20 0;2 4 0;16 13 0;21 26 0;7 25 0;21 14 0;11 16 0");
+		instancias.add("13 2 0;15 19 0;24 12 0;2 27 0;24 22 0;5 16 0;18 28 0;23 12 0;2 8 0;6 26 0;20 28 0;8 24 0;4 17 0;26 16 0;15 12 0;26 13 0;5 4 0;25 12 0;26 28 0;26 1 0;15 27 0;24 2 0;21 14 0;24 28 0;2 18 0;9 4 0;3 5 0;17 15 0");
+		instancias.add("4 20 0;9 12 0;17 29 0;12 25 0;29 27 0;27 19 0;26 26 0;16 27 0;4 12 0;19 6 0;18 11 0;7 4 0;29 24 0;16 7 0;3 14 0;26 23 0;25 5 0;9 22 0;2 12 0;11 3 0;28 1 0;23 19 0;24 16 0;15 23 0;28 18 0;29 22 0;1 1 0;19 27 0;5 26 0");
+		instancias.add("22 3 0;2 11 0;17 21 0;15 5 0;29 7 0;23 9 0;7 16 0;18 29 0;24 11 0;10 28 0;30 2 0;15 27 0;12 18 0;25 23 0;7 10 0;2 11 0;23 3 0;16 9 0;23 28 0;3 20 0;18 25 0;28 3 0;21 11 0;13 1 0;14 21 0;13 24 0;13 6 0;11 16 0;12 26 0;19 8 0");
+		
+		for(int i = 0; i < instancias.size(); i++){
+			int milisegundosTot = 0;
+			//Grafo gE =  ej2.crearGrafoDesdeInstancia(instancias.get(i));
+			Grafo gG =  ej2.crearGrafoDesdeInstancia(instancias.get(i));
+			Ej3.ciclos=0;
+			System.out.print(gG.getVertices().size());
+			long milisegundosactuales,milisegundos;
+			
+			for(int j = 0; j < 1000;j++){
+				gG =  ej2.crearGrafoDesdeInstancia(instancias.get(i));
+				milisegundosactuales = System.nanoTime();
+				@SuppressWarnings("unused")
+				List<Grafo.Vertice> conjG = Ej3.MCD_Greedy(gG,1);
+				milisegundosTot += System.nanoTime() - milisegundosactuales;
+			}
+			System.out.println(" "+ milisegundosTot/1000);
+	        
+		}
+		for(int i = 0; i < instancias.size(); i++){
+			long milisegundosTot = 0;
+			Grafo gE =  ej2.crearGrafoDesdeInstancia(instancias.get(i));
+			Ej3.ciclos=0;
+			System.out.print(gE.getVertices().size());
+			long milisegundosactuales,milisegundos;
+			
+			for(int j = 0; j < 1000;j++){
+				gE =  ej2.crearGrafoDesdeInstancia(instancias.get(i));
+				milisegundosactuales = System.nanoTime();
+				@SuppressWarnings("unused")
+List<Grafo.Vertice> conjE = ej2.obtenerConjuntoDominanteMinimo(gE); 				
+				milisegundosTot += System.nanoTime() - milisegundosactuales;
+			}
+			System.out.println(" "+ milisegundosTot/1000);
+			
+			
+			
+	        
+		}
+	}
 }
